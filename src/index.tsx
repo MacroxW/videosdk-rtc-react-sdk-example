f0b24073d58ff996b8f88fba5032fc91c8246deb
Marcos <wendymarcos2@gmail.com>
2025-12-17 11:48:07 -0300

    Migrate from react-scripts to Vite and convert all files to TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <>
     <ToastContainer
